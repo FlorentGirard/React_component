4287b37ecd4d949871ac2faf9410cbfdc6e28b7e
Florent Girard <mrgirardflorent@gmail.com>
2021-11-29 16:59:06 +0100

    feat(field):install bulma + styles component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,15 @@
 import Field from '../Field'
 
 //css
-
+import "bulma/css/bulma.min.css";
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
-    <Field placeholder="Login" name="login"/>
-  
+      <Field placeholder="Login" name="login" icon="fas fa-envelope" />
+      <Field placeholder="Test" name="login" />
+      
     </div>
   );
 }
--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,27 +1,44 @@
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
 import "./styles.scss";
 
 
-const Field = ({ type, placeholder, name }) => {
-    // const
+const Field = ({ type, placeholder, name, icon, isLeft}) => {
+    const className = classNames( {'control has-icons-left' : icon})
+  
     return (
-  <div className="field">
-    <label className="field__label"> {placeholder}</label>
-    <input placeholder={placeholder} name={name} type={type} />
-  </div>
-);
+      <div className="field">
+        <label className="field__label"> {placeholder}</label>
+        <div className={className}>
+          <input
+            placeholder={placeholder}
+            name={name}
+            type={type}
+            className="input "
+          />
+          {icon && (
+            <span className="icon is-small is-left">
+              <i className={icon}></i>
+            </span>
+          )}
+        </div>
+      </div>
+    );
 };
 
 Field.defaultProps = {
   type: "text",
   placeholder: "",
+  isLeft: true
 };
 
 Field.propTypes = {
   type: PropTypes.string,
   placeholder: PropTypes.string,
   name: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  isLeft: PropTypes.bool
 };
 
 export default Field;
