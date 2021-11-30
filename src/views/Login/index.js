ddeb020d7a51af170949a9f04657eada01c87bd3
Florent Girard <mrgirardflorent@gmail.com>
2021-11-30 15:41:54 +0100

    style(login): button styles

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,46 +1,93 @@
 import { Link } from "react-router-dom";
+import classNames from "classnames";
+
+
 
 import Field from "../../components/Field";
 
 import "./styles.scss";
 
-const Login = () => (
-  <div className="login">
-    <div className="login__container">
-      <h1 className="login__title">Login </h1>
-      <h2 className="login__subtitle">Déjà client ? </h2>
-      <form>
-        <Field
-          placeholder="Email"
-          name="email"
-          label="email"
-          type="email"
-          icon="fas fa-envelope"
-        />
-        <Field
-          placeholder="Mot de passe"
-          name="passworld"
-          label="passworld"
-          type="passworld"
-          icon="fas fa-lock"
-        />
+const Login = ({isRegister}) => {
+  const classNameRegister = classNames({ "login__input--hide": isRegister });
+  return (
+    <div className="login">
+      <div className="login__container">
+        <h1 className="login__title">Login </h1>
+        <h2 className="login__subtitle">Déjà client ? </h2>
+        <form>
+          <Field
+            placeholder="Name"
+            name="nom"
+            label="name"
+            icon="fas fa-user-tie"
+            className={classNameRegister}
+          />
+          <Field
+            placeholder="Prenom"
+            name="prenom"
+            label="prenom"
+            icon="fas fa-user-tie"
+            className={classNameRegister}
+          />
+          <Field
+            placeholder="Ville"
+            name="city"
+            label="prenom"
+            icon="fas fa-city"
+            className={classNameRegister}
+          />
 
-        <Link to="#" className="login__forgotPassworld">
-          {" "}
-          Mot de passe oublié ?{" "}
-        </Link>
-        <button className="login__button login__button--login">
+          <Field
+            placeholder="Email"
+            name="email"
+            label="email"
+            type="email"
+            icon="fas fa-envelope"
+          />
+          <Field
+            placeholder="Mot de passe"
+            name="passworld"
+            label="passworld"
+            type="passworld"
+            icon="fas fa-lock"
+          />
+          <Field
+            placeholder="Confirme mot de passe"
+            name="passworld"
+            label="passworld"
+            type="passworld"
+            icon="fas fa-lock"
+            className={classNameRegister}
+          />
+
+          <Link to="#" className="login__forgotPassworld">
+            Mot de passe oublié ?
+          </Link>
+          {/* button qui envoie donné */}
+          <button className="login__button login__button--login">
+            Se connecter
+          </button>
+          <button
+            className="login__button login__button--login "
+            
+          >
+            Créer un compte
+          </button>
+        </form>
+      </div>
+      <div className=" login__container login__registerContainer">
+        <h2 className="login__subtitle">Nouveau client ? </h2>
+
+        {/* button pour gérer l'affichage  */}
+        <button className="login__button login__button--register ">
+          Créer un compte
+        </button>
+        <button className="login__button login__button--register ">
           Se connecter
         </button>
-      </form>
+      </div>
     </div>
-    <div className=" login__container login__registerContainer">
-      <h2 className="login__subtitle">Nouveau client ? </h2>
-      <button className="login__button login__button--register ">
-        Créer un compte
-      </button>
-    </div>
-  </div>
-);
+  );
+  }
 
 export default Login;
